31c935474c51cf139f1de7051e790d149d5bedd7
kevinxuelei <kevinxuelei@163.com>
2021-08-23 15:43:24 +0800

    add project

---
+++ ios/YuntxIMKit/YuntxIMKit.h
@@ -0,0 +1,16 @@
+//
+//  YuntxIMKit.h
+//  YuntxIMKit
+//
+//  Created by dongxuelei on 2021/8/23.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+
+//#import <React/RCTBridgeModule.h>
+//#import <React/RCTLog.h>
+//#import <React/RCTEventDispatcher.h>
+@interface YuntxIMKit : NSObject<RCTBridgeModule>
+
+@end
